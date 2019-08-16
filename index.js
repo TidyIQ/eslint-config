8a88318dfc16fcf34ca1e6da02ce480669ef1fc6
unknown <jake@goldcoastmaids.com.au>
2019-08-16 12:04:12 +0700

    feat: ignore rest siblings from no-unused-vars

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ module.exports = {
         ]
       }
     ],
+    "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
     "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
     "react/prop-types": [0]
   },
